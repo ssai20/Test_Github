f1fe1f159d0a42c5921d1e1d8d52d9a5de833791
Shagaev <ssairan20@gmail.com>
2024-04-13 01:56:53 +0600

    second test commit

--- Hello_GitHub.java
+++ Hello_GitHub.java
@@ -1,4 +1,8 @@
 public class Hello_GitHub {
+    int a;
+    public Hello_GitHub(int a){
+        this.a = a;
+    }
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
     }
